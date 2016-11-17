548d29a10384b826ea001630e0a13cb2af7dbc42
Olof Dahlbom <olof.dahlbom@me.com>
2016-11-17 12:44:49 +0100

    Removed padding

--- debug-list-view.js
+++ debug-list-view.js
@@ -267,7 +267,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: SECONDARY_COLOR,
-    paddingTop: 20
+    paddingTop: 5
   },
   toolBar: {
     backgroundColor: SECONDARY_COLOR,
