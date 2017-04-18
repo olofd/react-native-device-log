e1bbc0c95a51dbff5f7690e966871a5c09dd840f
Olof Dahlbom <olof.dahlbom@me.com>
2017-04-18 19:38:01 +0200

    Fix for unmounting component

--- debug-view.js
+++ debug-view.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import DebugListView from './debug-list-view.js';
 import debugService from './debug-service';
 import debounce from 'debounce';
@@ -11,18 +11,22 @@ export default class DebugView extends React.Component {
     this.state = {
       rows: []
     };
+    this.unmounted = false;
     this.updateDebounced = debounce(this.update.bind(this), 150);
   }
 
   componentWillUnmount() {
+    this.unmounted = true;
     if (this.listner) {
       this.listner();
     }
   }
 
-  update(data){
+  update(data) {
     if (data) {
-      this.setState({rows: data.rows});
+      if (!this.unmounted) {
+        this.setState({ rows: data.rows });
+      }
     }
   }
 
