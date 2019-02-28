4030843d8d2157394a4dbd6532f16281048b4a37
Sahil Hussain <sahil.hussain113@gmail.com>
2019-02-28 12:04:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 export { default as LogView } from "./debug-view";
-import deviceLog from "./debug-service";
 export { default as InMemoryAdapter } from "./adapters/in-memory";
 export {
     default as StorageServerHocWriter,
-} from "./data-writers/storage-server-hoc-writer";
-export default deviceLog;
+}
+from "./data-writers/storage-server-hoc-writer";
+export { default as deviceLog } from "./debug-service";
+export { default as debugListView } from "./debug-list-view";
 export {
     default as StringifyDataWriter,
-} from "./data-writers/stringify-data-writer";
+}
+from "./data-writers/stringify-data-writer";
 export {
     default as RealmDataWriter,
     RealmBaseScheme,
-} from "./data-writers/realm-data-writer";
+}
+from "./data-writers/realm-data-writer";
