aafc798972e22e02023ee76e4a80fb2f8e021f51
Sahil Hussain <sahil.hussain113@gmail.com>
2019-03-01 16:57:25 +0530

    Update index.js
    Indentation Changed

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ export { default as LogView } from "./debug-view";
 export { default as InMemoryAdapter } from "./adapters/in-memory";
 export {
     default as StorageServerHocWriter,
-}
-from "./data-writers/storage-server-hoc-writer";
+} from "./data-writers/storage-server-hoc-writer";
 export { default as deviceLog } from "./debug-service";
 export { default as debugListView } from "./debug-list-view";
 export {
     default as StringifyDataWriter,
-}
-from "./data-writers/stringify-data-writer";
+} from "./data-writers/stringify-data-writer";
 export {
     default as RealmDataWriter,
     RealmBaseScheme,
-}
-from "./data-writers/realm-data-writer";
+} from "./data-writers/realm-data-writer";
